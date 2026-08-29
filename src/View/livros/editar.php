a7188308817e241d35d28e6631c04b5d774b113f
Deyse Guedes <deyseguedes5@gmail.com>
2026-08-29 09:05:36 -0300

    correcao da variavel que ta view, passando os parametros de ?? operador de coelecencia

--- src/View/livros/editar.php
+++ src/View/livros/editar.php
@@ -1,3 +1,11 @@
+<?php
+$livro = $livro ?? [
+    'id' => '',
+    'titulo' => '',
+    'autor' => '',
+    'ano_publicacao' => '',
+];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,15 +16,15 @@
 <body>
     <h1>Editar Livro</h1>
     <form action="<?= defined('BASE_URL') ? BASE_URL : '' ?>/livros/editar" method="POST">
-        <input type="hidden" name="id" value="<?= htmlspecialchars($livro['id'], ENT_QUOTES, 'UTF-8') ?>">
+        <input type="hidden" name="id" value="<?= htmlspecialchars($livro['id'] ?? '', ENT_QUOTES, 'UTF-8') ?>">
         <label for="nome">Nome do Livro</label>
-        <input type="text" id="nome" name="nome" value="<?= htmlspecialchars($livro['titulo'], ENT_QUOTES, 'UTF-8') ?>">
+        <input type="text" id="nome" name="nome" value="<?= htmlspecialchars($livro['titulo'] ?? '', ENT_QUOTES, 'UTF-8') ?>">
         <br><br>
         <label for="autor">Autor</label>
-        <input type="text" id="autor" name="autor" value="<?= htmlspecialchars($livro['autor'], ENT_QUOTES, 'UTF-8') ?>">
+        <input type="text" id="autor" name="autor" value="<?= htmlspecialchars($livro['autor'] ?? '', ENT_QUOTES, 'UTF-8') ?>">
         <br><br>
         <label for="ano">Ano de Publicação</label>
-        <input type="number" id="ano" name="ano" value="<?= htmlspecialchars((string) $livro['ano_publicacao'], ENT_QUOTES, 'UTF-8') ?>">
+        <input type="number" id="ano" name="ano" value="<?= htmlspecialchars((string) ($livro['ano_publicacao'] ?? ''), ENT_QUOTES, 'UTF-8') ?>">
         <br><br>
         <button type="submit">Salvar Alterações</button>
     </form>
